6fdeeb9a208277ec69e88c15b8139a30673ec252
Talut Salako <salakotalut@gmail.com>
2024-04-09 02:22:50 +0100

    test for post service

--- src/services/post_service.test.js
+++ src/services/post_service.test.js
@@ -1,24 +1,49 @@
-// import { MongoMemoryServer } from "mongodb-memory-server";
-// import mongoose from "mongoose";
-// import { expect, jest, test } from "@jest/globals";
-// import { Post } from "./post_service.js";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
 
-// describe("post service functions", () => {
-//   beforeAll(async () => {
-//     const mongod = await MongoMemoryServer.create();
-//     await mongoose.connect(mongod.getUri());
-//   });
+import { expect, jest } from "@jest/globals";
+import { createPost } from "./post_service.js";
+import Post from "../database/schema/post_schema.js";
+import User from "../database/schema/user_schema.js";
 
-//   afterAll(async () => {
-//     await mongoose.disconnect();
-//     await mongoose.connection.close();
-//   });
+jest.mock("../database/schema/post_schema.js", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
-//   describe("create post function", () => {
-//     it("should create and save a new post", () => {
-//       const title = "Test post";
-//       const body = "This is the content body";
-//       const user;
-//     });
-//   });
-// });
+describe("post service functions", () => {
+  let saveSpy, populateSpy;
+
+  beforeEach(() => {
+    // Create a mock instance of the Post model
+    const postInstance = new Post();
+
+    // Spy on the `save` method of the mock instance
+    saveSpy = jest.spyOn(postInstance, "save");
+    populateSpy = jest.spyOn(Post.prototype, "populate");
+  });
+  afterEach(() => {
+    saveSpy.mockRestore();
+  });
+
+  beforeAll(async () => {
+    const mongod = await MongoMemoryServer.create();
+    await mongoose.connect(mongod.getUri());
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("create post function", () => {
+    it("should create and save a new post", async () => {
+      const title = "Test post";
+      const body = "This is the content body";
+      const user = "661279f79f11a56b4bcf6da2";
+      await createPost(title, body, user);
+      expect(Post).toHaveBeenCalledWith({ title, body, user });
+      expect(saveSpy).toHaveBeenCalled();
+      expect(populateSpy).toHaveBeenCalledWith("user", "");
+    });
+  });
+});
